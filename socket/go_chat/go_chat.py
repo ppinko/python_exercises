6733d0d2ca2fb79605666529df81922de8d86929
ppinko <p.pinkowicz@gmail.com>
2020-06-21 11:37:20 +0200

    Adding functionality to run chat from one script.

--- socket/go_chat/go_chat.py
+++ socket/go_chat/go_chat.py
@@ -1,6 +1,7 @@
 if __name__ == "__main__":
     import os
     import sys
+    IP, PORT = "", ""
     input_arguments = len(sys.argv)
     if input_arguments > 1:
         for i, arg in enumerate(sys.argv):
@@ -12,11 +13,14 @@ if __name__ == "__main__":
                     print("No argument provided")
                 else:
                     print(f"IP {IP}, PORT {PORT}")
+                    os.system(
+                        "gnome-terminal -e 'bash -c \"python3 go_chat_server.py {0} {1}; bash\" '".format(IP, PORT))
+                    os.system(
+                        "gnome-terminal -e 'bash -c \"python3 go_chat_client.py {0} {1}; bash\" '".format(IP, PORT))
+                    os.system(
+                        "gnome-terminal -e 'bash -c \"python3 go_chat_client.py {0} {1}; bash\" '".format(IP, PORT))
     else:
         print("Server argument no provided")
 
-    os.system("gnome-terminal -e 'bash -c \"python3 go_chat_server.py; bash\" '")
-    os.system("gnome-terminal -e 'bash -c \"python3 go_chat_client.py; bash\" '")
-    os.system("gnome-terminal -e 'bash -c \"python3 go_chat_client.py; bash\" '")
-
+# s.connect((socket.gethostname(), 1243))
 
