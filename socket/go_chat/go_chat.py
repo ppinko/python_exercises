0056d27c93580807a1f91fd4f5db98b494e2bec9
ppinko <p.pinkowicz@gmail.com>
2020-06-21 10:51:54 +0200

    Adding file to start everythin - go_chat.py

---
+++ socket/go_chat/go_chat.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("gnome-terminal -e 'bash -c \"python3 go_chat_server.py; bash\" '")
+os.system("gnome-terminal -e 'bash -c \"python3 go_chat_client.py; bash\" '")
+os.system("gnome-terminal -e 'bash -c \"python3 go_chat_client.py; bash\" '")
