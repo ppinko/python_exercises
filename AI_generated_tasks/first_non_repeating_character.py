dd92da0c6bbd83597cc7c29147e41d9c15ba985e
ppinko <p.pinkowicz@gmail.com>
2025-03-16 16:02:48 +0100

    EXTEND - Add sophisticated solution using Counter and next iterator for first non repeating character task

--- AI_generated_tasks/first_non_repeating_character.py
+++ AI_generated_tasks/first_non_repeating_character.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 '''
 Challenge: First Non-Repeating Character
 Given a string, find the first non-repeating character and return its index. 
@@ -40,3 +42,13 @@ s3 = "aNanas"
 print(first_non_repeating_character(s1))
 print(first_non_repeating_character(s2))
 print(first_non_repeating_character(s3))
+
+def first_unique_char(s):
+    char_count = Counter(s)  # Count character frequencies
+
+    # Single pass to find the first unique character
+    return next((i for i, char in enumerate(s) if char_count[char] == 1), -1)
+
+print(first_unique_char(s1))  # Output: 0
+print(first_unique_char(s2))  # Output: -1
+print(first_unique_char(s3))  # Output: 1, solution is case sensitive
