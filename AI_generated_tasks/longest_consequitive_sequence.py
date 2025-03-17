1831e3539b78ad5cb3f4ec393bef56443bb76e94
ppinko <p.pinkowicz@gmail.com>
2025-03-17 11:56:44 +0100

    ADD - Add more efficient solution for longest consequitive sequence task

--- AI_generated_tasks/longest_consequitive_sequence.py
+++ AI_generated_tasks/longest_consequitive_sequence.py
@@ -44,3 +44,36 @@ def longest_consequitive_sequence_v1(nums: list[int]) -> int:
 
 assert longest_consequitive_sequence_v1([100, 4, 200, 1, 3, 2]) == 4
 assert longest_consequitive_sequence_v1([9, 1, 4, 7, 3, -2, 0, 5, 8, -1, 6]) == 7
+
+
+def longest_consequitive_sequence_v2(nums: list[int]) -> int:
+    """
+    Use set for efficient look-up and elements removal.
+    """
+    s = set(nums)
+    counter, highest = 0, 0
+    while len(s) != 0:
+        v = s.pop()
+        counter += 1
+        # check left side
+        temp = v - 1
+        while temp in s:
+            counter += 1
+            s.remove(temp)
+            temp -= 1
+
+        # check left side
+        temp = v + 1
+        while temp in s:
+            counter += 1
+            s.remove(temp)
+            temp += 1
+
+        if counter > highest:
+            highest = counter
+        counter = 0  # reset counter
+    return highest
+
+
+assert longest_consequitive_sequence_v2([100, 4, 200, 1, 3, 2]) == 4
+assert longest_consequitive_sequence_v2([9, 1, 4, 7, 3, -2, 0, 5, 8, -1, 6]) == 7
